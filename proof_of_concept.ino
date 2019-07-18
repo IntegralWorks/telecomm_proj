98582dbe88e6a5c521597c4005ff6db66b24406d
Javi Salazar <javisalazar2@gmail.com>
2019-07-18 01:14:51 -0500

    Update proof_of_concept.ino
    Final version for presentation--barely attains Proof of Concept but it does

--- proof_of_concept.ino
+++ proof_of_concept.ino
@@ -1,73 +1,325 @@
- /*
- * The Circuit: 
- * Connect AUD to analog input 0
- * Connect GND to GND
- * Connect VCC to 3.3V (3.3V yields the best results)
- *  
- *  To adjust when the LED turns on based on audio input:
- *  Open up the serial com port (Top right hand corner of the Arduino IDE)
- *  It looks like a magnifying glass. Perform several experiments 
- *  clapping, snapping, blowing, door slamming, knocking etc and see where the
- *  resting noise level is and where the loud noises are. Adjust the if statement
- *  according to your findings.
- *  
- *  You can also adjust how long you take samples for by updating the "SampleWindow"
- * 
- * This code has been adapted from the
- * Example Sound Level Sketch for the
- * Adafruit Microphone Amplifier
- * 
- */
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ArduinoOTA.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <FS.h>
+#include <WebSocketsServer.h>
+
+ESP8266WiFiMulti wifiMulti;       // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+
+ESP8266WebServer server(80);       // Create a webserver object that listens for HTTP request on port 80
+WebSocketsServer webSocket(81);    // create a websocket server on port 81
+
+File fsUploadFile;                 // a File variable to temporarily store the received file
+
+const char *ssid = "ESP8266 Access Point"; // The name of the Wi-Fi network that will be created
+const char *password = "thereisnospoon";   // The password required to connect to it, leave blank for an open network
+
+const char *OTAName = "ESP8266";           // A name and a password for the OTA service
+const char *OTAPassword = "esp8266";
+
+const char* mdnsName = "esp8266"; // Domain name for the mDNS responder
+
+#define USE_SERIAL Serial1
+
+void setup() {
+  audio_setup();
+
+  Serial.begin(115200);        // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println("\r\n");
+
+  startWiFi();                 // Start a Wi-Fi access point, and try to connect to some given access points. Then wait for either an AP or STA connection
+
+  startSPIFFS();               // Start the SPIFFS and list all contents
+
+  startWebSocket();            // Start a WebSocket server
+
+  startMDNS();                 // Start the mDNS responder
+
+  startServer();               // Start a HTTP server with a file read handler and an upload handler
+
+}
+
+
+
+bool rainbow = false;             // The rainbow effect is turned off on startup
+
+unsigned long prevMillis = millis();
+int hue = 0;
+
+void loop() {
+  webSocket.loop();                           // constantly check for websocket events
+  server.handleClient();                      // run the server
+  audio_loop();
+
+
+  //if (Serial.available() > 0)
+//    char c[] = {(char)Serial.read()};
+//    webSocket.broadcastTXT(c, sizeof(c));
+ 
+}
+
+
+
+void startWiFi() { // Start a Wi-Fi access point, and try to connect to some given access points. Then wait for either an AP or STA connection
+  WiFi.softAP(ssid, password);             // Start the access point
+  Serial.print("Access Point \"");
+  Serial.print(ssid);
+  Serial.println("\" started\r\n");
+
+  wifiMulti.addAP("ssid_from_AP_1", "your_password_for_AP_1");   // add Wi-Fi networks you want to connect to
+  wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
+  wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
+
+  Serial.println("Connecting");
+  while (wifiMulti.run() != WL_CONNECTED && WiFi.softAPgetStationNum() < 1) {  // Wait for the Wi-Fi to connect
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println("\r\n");
+  if (WiFi.softAPgetStationNum() == 0) {     // If the ESP is connected to an AP
+    Serial.print("Connected to ");
+    Serial.println(WiFi.SSID());             // Tell us what network we're connected to
+    Serial.print("IP address:\t");
+    Serial.print(WiFi.localIP());            // Send the IP address of the ESP8266 to the computer
+  } else {                                   // If a station is connected to the ESP SoftAP
+    Serial.print("Station connected to ESP8266 AP");
+  }
+  Serial.println("\r\n");
+}
+
+void startSPIFFS() { // Start the SPIFFS and list all contents
+  SPIFFS.begin();                             // Start the SPI Flash File System (SPIFFS)
+  Serial.println("SPIFFS started. Contents:");
+  {
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next()) {                      // List the file system contents
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      Serial.printf("\tFS File: %s, size: %s\r\n", fileName.c_str(), formatBytes(fileSize).c_str());
+    }
+    Serial.printf("\n");
+  }
+}
+
+void startWebSocket() { // Start a WebSocket server
+  webSocket.begin();                          // start the websocket server
+  webSocket.onEvent(webSocketEvent);          // if there's an incomming websocket message, go to function 'webSocketEvent'
+  Serial.println("WebSocket server started.");
+}
+
+void startMDNS() { // Start the mDNS responder
+  MDNS.begin(mdnsName);                        // start the multicast domain name server
+  Serial.print("mDNS responder started: http://");
+  Serial.print(mdnsName);
+  Serial.println(".local");
+}
+
+void startServer() { // Start a HTTP server with a file read handler and an upload handler
+  server.on("/edit.html",  HTTP_POST, []() {  // If a POST request is sent to the /edit.html address,
+    server.send(200, "text/plain", "");
+  }, handleFileUpload);                       // go to 'handleFileUpload'
+
+  server.onNotFound(handleNotFound);          // if someone requests any other file or page, go to function 'handleNotFound'
+  // and check if the file exists
+
+  server.begin();                             // start the HTTP server
+  Serial.println("HTTP server started.");
+}
+
+
+
+void handleNotFound() { // if the requested file or page doesn't exist, return a 404 not found error
+  if (!handleFileRead(server.uri())) {        // check if the file exists in the flash memory (SPIFFS), if so, send it
+    server.send(404, "text/plain", "404: File Not Found");
+  }
+}
+
+bool handleFileRead(String path) { // send the right file to the client (if it exists)
+  Serial.println("handleFileRead: " + path);
+  if (path.endsWith("/")) path += "index.html";          // If a folder is requested, send the index file
+  String contentType = getContentType(path);             // Get the MIME type
+  String pathWithGz = path + ".gz";
+  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) { // If the file exists, either as a compressed archive, or normal
+    if (SPIFFS.exists(pathWithGz))                         // If there's a compressed version available
+      path += ".gz";                                         // Use the compressed verion
+    File file = SPIFFS.open(path, "r");                    // Open the file
+    size_t sent = server.streamFile(file, contentType);    // Send it to the client
+    file.close();                                          // Close the file again
+    Serial.println(String("\tSent file: ") + path);
+    return true;
+  }
+  Serial.println(String("\tFile Not Found: ") + path);   // If the file doesn't exist, return false
+  return false;
+}
+
+void handleFileUpload() { // upload a new file to the SPIFFS
+  HTTPUpload& upload = server.upload();
+  String path;
+  if (upload.status == UPLOAD_FILE_START) {
+    path = upload.filename;
+    if (!path.startsWith("/")) path = "/" + path;
+    if (!path.endsWith(".gz")) {                         // The file server always prefers a compressed version of a file
+      String pathWithGz = path + ".gz";                  // So if an uploaded file is not compressed, the existing compressed
+      if (SPIFFS.exists(pathWithGz))                     // version of that file must be deleted (if it exists)
+        SPIFFS.remove(pathWithGz);
+    }
+    Serial.print("handleFileUpload Name: "); Serial.println(path);
+    fsUploadFile = SPIFFS.open(path, "w");            // Open the file for writing in SPIFFS (create if it doesn't exist)
+    path = String();
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    if (fsUploadFile)
+      fsUploadFile.write(upload.buf, upload.currentSize); // Write the received bytes to the file
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (fsUploadFile) {                                   // If the file was successfully created
+      fsUploadFile.close();                               // Close the file again
+      Serial.print("handleFileUpload Size: "); Serial.println(upload.totalSize);
+      server.sendHeader("Location", "/success.html");     // Redirect the client to the success page
+      server.send(303);
+    } else {
+      server.send(500, "text/plain", "500: couldn't create file");
+    }
+  }
+}
+double global = 0;
+//bool loop_trigger = false;
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+
+  switch (type) {
+    case WStype_DISCONNECTED:
+      USE_SERIAL.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        USE_SERIAL.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
+        // send message to client
+        webSocket.sendTXT(num, "Connected");
+       
+      }
+      break;
+    case WStype_TEXT: {
+        //USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
+
+        // send message to client
+        // webSocket.sendTXT(num, "message here");
+
+        //for (int i = 0; i < length; i++) Serial.print((char) payload[i]);
+        //Serial.println("WE GOT YOUR TEXT!");
+        // send data to all connected clients
+        
+        //loop_trigger = !loop_trigger;
+
+        
+        int counter = 10;
+        for (int i = 0; i < counter; ++i){
+          String converted_global = String(global);
+          webSocket.broadcastTXT(converted_global + "\n");
+          delay(100);
+        }
+//        while(loop_trigger) {
+//
+//        String converted_global = String(global);
+//        webSocket.broadcastTXT(converted_global + "\n");
+//        delay(500);
+//          
+//        }
+//        String converted_global = String(global);
+//        webSocket.broadcastTXT(converted_global);
+        
+        break;
+      }
+    case WStype_BIN:
+      USE_SERIAL.printf("[%u] get binary length: %u\n", num, length);
+      hexdump(payload, length);
+
+      // send message to client
+      // webSocket.sendBIN(num, payload, length);
+      break;
+  }
+
+}
+
+
+String formatBytes(size_t bytes) { // convert sizes in bytes to KB and MB
+  if (bytes < 1024) {
+    return String(bytes) + "B";
+  } else if (bytes < (1024 * 1024)) {
+    return String(bytes / 1024.0) + "KB";
+  } else if (bytes < (1024 * 1024 * 1024)) {
+    return String(bytes / 1024.0 / 1024.0) + "MB";
+  }
+}
+
+String getContentType(String filename) { // determine the filetype of a given filename, based on the extension
+  if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+//############################################
+// From here is the telecommunications project:
 
 const int sampleWindow = 250; // Sample window width in mS (250 mS = 4Hz)
 unsigned int knock;
-int ledPin = 13; //D7
+int piezoPin = 13; //D7
 
-void setup() 
+void audio_setup()
 {
-   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  pinMode(piezoPin, OUTPUT);
 }
 
-void loop() 
+void audio_loop()
 {
- unsigned long start= millis();  // Start of sample window
- unsigned int peakToPeak = 0;   // peak-to-peak level
+  unsigned long start = millis(); // Start of sample window
+  unsigned int peakToPeak = 0;   // peak-to-peak level
 
- unsigned int signalMax = 0;
- unsigned int signalMin = 1024;
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1024;
 
- // collect data for 250 miliseconds
- while (millis() - start < sampleWindow)
- {
-   knock = analogRead(0);
-      if (knock < 1024)  //This is the max of the 10-bit ADC so this loop will include all readings
+  // collect data for 250 miliseconds
+  while (millis() - start < sampleWindow)
+  {
+    knock = analogRead(0); //A0
+    if (knock < 1024)  //This is the max of the 10-bit ADC so this loop will include all readings
+    {
+      if (knock > signalMax)
       {
-         if (knock > signalMax)
-         {
-           signalMax = knock;  // save just the max levels
-         }
+        signalMax = knock;  // save just the max levels
+      }
       else if (knock < signalMin)
-        {
-         signalMin = knock;  // save just the min levels
-         }
-     }
- }
- peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
- double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
+      {
+        signalMin = knock;  // save just the min levels
+      }
+    }
+  }
+  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+  double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
+
+  global = volts;
 
 
-Serial.println(volts);
- if (volts >=.02) //really low rn
- {
-  //turn on LED
-  digitalWrite(ledPin, HIGH);
-   delay(500);
-  Serial.println("Knock Knock");
- }
- else
- {
- //turn LED off
- digitalWrite(ledPin, LOW);
- }             
+  Serial.println(volts);
+  //server.send(volts);
+
+  if (volts >= .02)
+  {
+    //turn on LED
+    digitalWrite(piezoPin, HIGH);
+    delay(500);
+    Serial.println("Knock Knock");
+  }
+  else
+  {
+    //turn LED off
+    digitalWrite(piezoPin, LOW);
+    delay(500);
+  }
 }
